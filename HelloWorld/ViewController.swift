db61807fc2fbe6e1400b3f8bceceaa47312d37b8
Hung Lay <hunglay@Hungs-MacBook-Air.local>
2017-04-13 23:15:55 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Hung Lay on 4/13/17.
+//  Copyright © 2017 Hung Lay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
